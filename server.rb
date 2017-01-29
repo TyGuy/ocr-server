56e01b7b11cb15ab72ede9497bc313a5ca727be5
Tyler Davis <tee.s.davis@gmail.com>
2017-01-29 10:36:44 -0800

    Initial commit

---
+++ server.rb
@@ -0,0 +1,15 @@
+require 'sinatra/base'
+require './lib/lumberjack'
+
+class Server < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  include Lumberjack
+
+  use Rack::Auth::Basic do |username, password|
+    username == ENV['API_USER'] && password == ENV['API_PASSWORD']
+  end
+
+  get '/' do
+    'WOOOOOOO'
+  end
+end
